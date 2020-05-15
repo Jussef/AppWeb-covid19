8053609f1f391a499d4c56b3291c085d34748bf0
Jussefo91 <jussefspira@users.noreply.github.com>
2020-05-14 23:58:37 -0500

    El corona

--- app/src/main/java/com/example/chicarron/MainActivity.kt
+++ app/src/main/java/com/example/chicarron/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.chicarron
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.http.SslError
 import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    // Private
+
+    private val url = "https://coronavirus.app/tracking/mexico"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // WebView
+        webView.webChromeClient = object : WebChromeClient() {
+        }
+        webView.webViewClient = object : WebViewClient() {
+        }
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.allowContentAccess = true
+        settings.domStorageEnabled = true
+        settings.useWideViewPort = true
+        webView.loadUrl(url)
+    }
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
